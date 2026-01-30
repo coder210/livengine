fb8030bd1f11f425127151c06536e515fbca687b
code210 <228407354@qq.com>
2026-01-31 00:40:01 +0800

    update: 更新

--- cmake-build/3rd/SDL/include-revision/SDL3/SDL_revision.h
+++ cmake-build/3rd/SDL/include-revision/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL3-3.2.6-60927ab (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL3-3.2.6-2a43770 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL3-3.2.6-60927ab"
+#define SDL_REVISION "SDL3-3.2.6-2a43770"
 #endif
 
 #endif /* SDL_revision_h_ */
